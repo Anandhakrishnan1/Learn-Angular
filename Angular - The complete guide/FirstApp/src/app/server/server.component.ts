3d826dd108f50f697c560e39d6aea320b9fc1734
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-13 14:32:45 +0530

    changed selector to an attribute

--- Angular - The complete guide/FirstApp/src/app/server/server.component.ts
+++ Angular - The complete guide/FirstApp/src/app/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-server',
+  selector: '[app-server]',
   standalone: true,
   imports: [],
   templateUrl: './server.component.html',
