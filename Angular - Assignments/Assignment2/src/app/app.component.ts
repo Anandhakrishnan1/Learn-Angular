8ee92af08b06548225b8fc7934f4662398e80295
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-15 17:11:52 +0530

    Assignment-2

---
+++ Angular - Assignments/Assignment2/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app.routes';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, AppRoutingModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Assignment2';
+}
---
+++ Angular - Assignments/Assignment2/src/app/app.routes.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { ErrorComponent } from './error/error.component';
+
+export const routes: Routes = [{
+    path: 'list',
+    component: ListComponent,
+    title: 'List'
+},
+{
+    path: 'details',
+    component: DetailsComponent,
+    title: 'Details'
+},
+{
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+},
+{
+    path: '**',
+    component: ErrorComponent,
+    title: 'Error'
+}];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{ }
