6a1c2269414a88bb3692f3d08735f37070dadd67
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-18 15:03:38 +0530

    Added  edit functionality for the list

--- Angular - Assignments/Assignment2/src/app/details/details.component.ts
+++ Angular - Assignments/Assignment2/src/app/details/details.component.ts
@@ -1,17 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
+updateMovieName(index: number, $event: Event) {
+ this.selectedActorMovies[index] = ((<HTMLInputElement>$event.target).value);
+ this.isEditClicked = false;
+}
+updateMovieList() {
+  this.isEditClicked = false;
+}
+editMovieName(index: number) {
+  this.isEditClicked = true;
+  this.editingIndex = index;
+}
   id: string|number = 0;
+  selectedMovie: string = '';
   selectedActorMovies: string[] = [];
+  isEditClicked = false;
+  editingIndex: number = -1;
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(){
