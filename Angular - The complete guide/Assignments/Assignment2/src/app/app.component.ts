57869792efaddbfabefdff59425186d08e2607ba
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-22 16:48:29 +0530

    Completed assignment 2

--- Angular - The complete guide/Assignments/Assignment2/src/app/app.component.ts
+++ Angular - The complete guide/Assignments/Assignment2/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+isUserNameEmpty() {
+  if(this.username === '')
+    return true;
+  else
+    return false;
+}
   title = 'Assignment2';
+  username = ''
 }
