a64e8cab2dd0d034e1996dbf253666ad152976a0
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-24 13:20:28 +0530

    using every components

--- Angular - The complete guide/project1/src/app/app.component.ts
+++ Angular - The complete guide/project1/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent]
 })
 export class AppComponent {
   title = 'project1';
--- Angular - The complete guide/project1/src/app/shopping-list/shopping-list.component.ts
+++ Angular - The complete guide/project1/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 
 @Component({
-  selector: 'app-shopping-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+    selector: 'app-shopping-list',
+    standalone: true,
+    templateUrl: './shopping-list.component.html',
+    styleUrl: './shopping-list.component.css',
+    imports: [ShoppingEditComponent]
 })
 export class ShoppingListComponent {
 
