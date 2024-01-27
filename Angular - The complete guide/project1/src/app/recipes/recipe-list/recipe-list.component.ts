567ea1fc5a7f833a10799d2cc78e827f6561371f
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-27 15:12:44 +0530

    created new recipes object

--- Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
+import { Recipe } from '../models/recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -9,5 +10,7 @@ import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
     imports: [RecipeItemComponent]
 })
 export class RecipeListComponent {
-
+    recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'Test description', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg')
+    ];
 }
