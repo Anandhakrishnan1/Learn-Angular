8bd981c8998f1e7b4b90185f218f07da27cae42c
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-27 15:32:01 +0530

    dynamic data binding using ngfor

--- Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
 import { Recipe } from '../models/recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-recipe-list',
     standalone: true,
     templateUrl: './recipe-list.component.html',
     styleUrl: './recipe-list.component.css',
-    imports: [RecipeItemComponent]
+    imports: [RecipeItemComponent, CommonModule]
 })
 export class RecipeListComponent {
     recipes: Recipe[] = [
