1afdcf1da27924faba793c72b826513ac847c647
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-27 15:34:20 +0530

    added more items to recipes list and verified ui response

--- Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class RecipeListComponent {
     recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'Test description', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg')
+        new Recipe('Recipe 1', 'description 1', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'),
+        new Recipe('Recipe 2', 'description 2', 'https://img.buzzfeed.com/buzzfeed-static/static/2020-05/27/18/asset/3af49fdc8a8c/sub-buzz-1600-1590605566-3.jpg')
     ];
 }
