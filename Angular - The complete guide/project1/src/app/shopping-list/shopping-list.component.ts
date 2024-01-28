a4d12b161acc5b237d45401c258f7f21327d9c2b
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-28 12:40:33 +0530

    integrated ingredient in shopping list ui

--- Angular - The complete guide/project1/src/app/shopping-list/shopping-list.component.ts
+++ Angular - The complete guide/project1/src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { Ingredient } from '../recipes/models/ingredient.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-shopping-list',
     standalone: true,
     templateUrl: './shopping-list.component.html',
     styleUrl: './shopping-list.component.css',
-    imports: [ShoppingEditComponent]
+    imports: [ShoppingEditComponent, CommonModule]
 })
 export class ShoppingListComponent {
     ingredients: Ingredient[] = [
