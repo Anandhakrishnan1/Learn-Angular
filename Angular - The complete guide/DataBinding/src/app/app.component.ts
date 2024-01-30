3f7f7332e20cca4ec6b0b3e47b28d6585231c131
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-30 19:01:33 +0530

    designed cockpit

--- Angular - The complete guide/DataBinding/src/app/app.component.ts
+++ Angular - The complete guide/DataBinding/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CockpitComponent } from "./cockpit/cockpit.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CockpitComponent]
 })
 export class AppComponent {
   title = 'DataBinding';
--- Angular - The complete guide/DataBinding/src/app/cockpit/cockpit.component.ts
+++ Angular - The complete guide/DataBinding/src/app/cockpit/cockpit.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cockpit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cockpit.component.html',
   styleUrl: './cockpit.component.css'
 })
 export class CockpitComponent {
+newServerName= '';
+newServerContent= '';
 
 }
