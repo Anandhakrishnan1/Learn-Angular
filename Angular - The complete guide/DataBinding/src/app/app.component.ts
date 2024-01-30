7fe4c0eaa34d78024f583bc8084fa6b3543a7c9e
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-01-30 19:41:34 +0530

    binding

--- Angular - The complete guide/DataBinding/src/app/app.component.ts
+++ Angular - The complete guide/DataBinding/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CockpitComponent } from "./cockpit/cockpit.component";
+import { ServerElementComponent } from "./server-element/server-element.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, CockpitComponent]
+    imports: [CommonModule, RouterOutlet, CockpitComponent, ServerElementComponent]
 })
 export class AppComponent {
   title = 'DataBinding';
+  serverElements = [];
 }
--- Angular - The complete guide/DataBinding/src/app/server-element/server-element.component.ts
+++ Angular - The complete guide/DataBinding/src/app/server-element/server-element.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css'
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit{
+  element: { type: string; name: string; content: string; } | undefined;
 
+  ngOnInit(): void {
+    
+  }
 }
