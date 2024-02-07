1a5ff1ab1e4219dee98a068dc95b0037a0f9c8bd
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-07 17:58:58 +0530

    assignment 4 completed

--- Angular - The complete guide/Assignments/Assignment4/src/app/app.component.ts
+++ Angular - The complete guide/Assignments/Assignment4/src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from "./odd/odd.component";
+import { EvenComponent } from "./even/even.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, GameControlComponent]
+    imports: [CommonModule, RouterOutlet, GameControlComponent, OddComponent, EvenComponent]
 })
 export class AppComponent {
   onIntervalFired($event: number) {
