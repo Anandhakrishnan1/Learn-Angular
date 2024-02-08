f97b3af5da2da07fee3bebfed2443b0d24f5d26f
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-08 17:59:08 +0530

    Passing recipe data with property binding

--- Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Angular - The complete guide/project1/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
-
+  @Input() recipe!: Recipe;
 }
