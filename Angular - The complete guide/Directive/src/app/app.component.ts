235b6d09b5250bb8059cc9b4472ae09d45d3a16a
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-13 12:35:39 +0530

    created first derivative

--- Angular - The complete guide/Directive/src/app/app.component.ts
+++ Angular - The complete guide/Directive/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BasicHighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
