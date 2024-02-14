6e17951c777510308e9d9df12932b8d173269fd3
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-14 12:59:04 +0530

    styling dynamically

--- Angular - The complete guide/Directive/src/app/odd-highlight/odd-highlight.directive.ts
+++ Angular - The complete guide/Directive/src/app/odd-highlight/odd-highlight.directive.ts
@@ -1,25 +1,28 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appOddHighlight]',
   standalone: true
 })
 export class OddHighlightDirective {
-  @HostBinding('style.backgroundColor') backgroundColor : string = 'blue';
+  @Input('defaultColor') defaultColor: string = 'blue';
+  @Input('highLightColor') highLightColor: string = 'grey';
+  @HostBinding('style.backgroundColor') backgroundColor : string = '';
 
   constructor(private renderer : Renderer2, private elementRef : ElementRef) { }
 
   ngOnInit(){
+    this.backgroundColor = this.defaultColor;
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
 
   @HostListener('mouseenter') mouseEnter(eventData: Event){
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'grey');
-    this.backgroundColor = 'grey';
+    this.backgroundColor = this.highLightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event){
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.defaultColor;
   }
 }
