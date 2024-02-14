22c0237da2c270f704d38129e79546d062b7f0a8
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-14 13:20:10 +0530

    building a structural directive

--- Angular - The complete guide/Directive/src/app/app.component.ts
+++ Angular - The complete guide/Directive/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { OddHighlightDirective } from './odd-highlight/odd-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BasicHighlightDirective, OddHighlightDirective],
+  imports: [CommonModule, RouterOutlet, BasicHighlightDirective, OddHighlightDirective, UnlessDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
