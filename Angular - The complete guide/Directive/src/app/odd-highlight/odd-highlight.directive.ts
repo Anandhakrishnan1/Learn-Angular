82852f82a063d807bf0660a1b7169b7e948c0776
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-14 12:41:04 +0530

    Using HostListener to listen to host events

--- Angular - The complete guide/Directive/src/app/odd-highlight/odd-highlight.directive.ts
+++ Angular - The complete guide/Directive/src/app/odd-highlight/odd-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appOddHighlight]',
@@ -9,6 +9,14 @@ export class OddHighlightDirective {
   constructor(private renderer : Renderer2, private elementRef : ElementRef) { }
 
   ngOnInit(){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseEnter(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'grey');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event){
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
 }
