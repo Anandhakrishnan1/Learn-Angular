ab070286fdc7f01f7a22a001686e46ed79bdd042
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-14 12:27:24 +0530

    created directive odd-highlight

---
+++ Angular - The complete guide/Directive/src/app/odd-highlight/odd-highlight.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appOddHighlight]',
+  standalone: true
+})
+export class OddHighlightDirective {
+
+  constructor() { }
+
+}
