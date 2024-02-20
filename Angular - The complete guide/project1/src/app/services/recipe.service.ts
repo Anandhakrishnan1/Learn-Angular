4dba2e795e731d88972264e41045b8750805f1e9
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-20 12:27:48 +0530

    created showRecipeDetails  eventEnitter

--- Angular - The complete guide/project1/src/app/services/recipe.service.ts
+++ Angular - The complete guide/project1/src/app/services/recipe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipes/models/recipe.model';
 
 @Injectable({
@@ -10,6 +10,8 @@ export class RecipeService {
     new Recipe('Recipe 1', 'description 1', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'),
     new Recipe('Recipe 2', 'description 2', 'https://img.buzzfeed.com/buzzfeed-static/static/2020-05/27/18/asset/3af49fdc8a8c/sub-buzz-1600-1590605566-3.jpg')
   ];
+
+  showRecipeDetails = new EventEmitter<Recipe>();
 
   constructor() { }
 
