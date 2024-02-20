f9df0ccae4d5973a682af10d74db3edf7e876030
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-20 12:29:11 +0530

    subscibed to showRecipeDetails  to listen to click event

--- Angular - The complete guide/project1/src/app/recipes/recipes.component.ts
+++ Angular - The complete guide/project1/src/app/recipes/recipes.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { Recipe } from './models/recipe.model';
 import { CommonModule } from '@angular/common';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
     selector: 'app-recipes',
@@ -11,11 +12,22 @@ import { CommonModule } from '@angular/common';
     styleUrl: './recipes.component.css',
     imports: [RecipeDetailComponent, RecipeListComponent, CommonModule]
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
     selectedRecipe!: Recipe;
+
+    constructor(private recipeService: RecipeService){}
     
-    showRecipeDetails(recipe: Recipe) {
-        this.selectedRecipe = recipe;
+    ngOnInit(): void {
+        this.recipeService.showRecipeDetails.subscribe(
+            (recipe: Recipe) => {
+              debugger
+              this.selectedRecipe = recipe
+            }
+          );
     }
+    
+    // showRecipeDetails(recipe: Recipe) {
+    //     this.selectedRecipe = recipe;
+    // }
 
 }
