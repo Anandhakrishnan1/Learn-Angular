2700ae3dbbd80a0b2e26a535c35f1dfccf093768
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-20 17:16:20 +0530

    displayed the ingrediants in recipe details view

--- Angular - The complete guide/project1/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Angular - The complete guide/project1/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
 import { DropdownDirective } from '../../shared/dropdown.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [DropdownDirective],
+  imports: [DropdownDirective, CommonModule],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
