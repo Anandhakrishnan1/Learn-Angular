38271c8830eabab987ce93cb24d58d1097382d94
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-20 17:15:55 +0530

    added values

--- Angular - The complete guide/project1/src/app/services/recipe.service.ts
+++ Angular - The complete guide/project1/src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipes/models/recipe.model';
+import { Ingredient } from '../recipes/models/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,8 @@ import { Recipe } from '../recipes/models/recipe.model';
 export class RecipeService {
 
   recipes: Recipe[] = [
-    new Recipe('Recipe 1', 'description 1', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'),
-    new Recipe('Recipe 2', 'description 2', 'https://img.buzzfeed.com/buzzfeed-static/static/2020-05/27/18/asset/3af49fdc8a8c/sub-buzz-1600-1590605566-3.jpg')
+    new Recipe('Recipe 1', 'description 1', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg',[new Ingredient('Meat',1) , new Ingredient('Bread',1)]),
+    new Recipe('Recipe 2', 'description 2', 'https://img.buzzfeed.com/buzzfeed-static/static/2020-05/27/18/asset/3af49fdc8a8c/sub-buzz-1600-1590605566-3.jpg', [new Ingredient('Meat',1) , new Ingredient('Bun',5)])
   ];
 
   showRecipeDetails = new EventEmitter<Recipe>();
