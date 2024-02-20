4869f338c5a87401ce1ff941e74fffa097a1d5bb
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-20 15:48:06 +0530

    created get method in service

--- Angular - The complete guide/project1/src/app/services/shopping-list.service.ts
+++ Angular - The complete guide/project1/src/app/services/shopping-list.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../recipes/models/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-
+  
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
   constructor() { }
+
+  get(){
+    return this.ingredients.slice();
+  }
 }
