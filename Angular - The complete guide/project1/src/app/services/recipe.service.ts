12e2da792aa662595c0aa92e1f902caf336c2ae4
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-20 11:56:08 +0530

    implementd recipe list in service

--- Angular - The complete guide/project1/src/app/services/recipe.service.ts
+++ Angular - The complete guide/project1/src/app/services/recipe.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/models/recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'description 1', 'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'),
+    new Recipe('Recipe 2', 'description 2', 'https://img.buzzfeed.com/buzzfeed-static/static/2020-05/27/18/asset/3af49fdc8a8c/sub-buzz-1600-1590605566-3.jpg')
+  ];
+
   constructor() { }
+
+  get(){
+    return this.recipes.slice();
+  }
 }
