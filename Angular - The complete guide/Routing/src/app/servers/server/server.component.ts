0c0b96f06c03cf9206dbfab579b1d022334ca953
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-26 15:32:06 +0530

    loading users reactively

--- Angular - The complete guide/Routing/src/app/servers/server/server.component.ts
+++ Angular - The complete guide/Routing/src/app/servers/server/server.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -11,9 +12,11 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit{
   server!: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1)!;
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id)!;
   }
 }
--- Angular - The complete guide/Routing/src/app/users/users.component.ts
+++ Angular - The complete guide/Routing/src/app/users/users.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { UserComponent } from "./user/user.component";
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-users',
     standalone: true,
     templateUrl: './users.component.html',
     styleUrl: './users.component.css',
-    imports: [UserComponent, CommonModule]
+    imports: [UserComponent, CommonModule, RouterLink]
 })
 export class UsersComponent {
   users = [
