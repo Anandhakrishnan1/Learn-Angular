fabbe20731e70cac38b94b8b3b559341ac8b4283
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-03-04 18:43:26 +0530

    Update edit-server.component.ts

--- Angular - The complete guide/Routing/src/app/servers/edit-server/edit-server.component.ts
+++ Angular - The complete guide/Routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CanComponentDeactivate } from '../../can-deactivate-guard.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
@@ -11,28 +13,45 @@ import { CommonModule } from '@angular/common';
   templateUrl: './edit-server.component.html',
   styleUrl: './edit-server.component.css'
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server!: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1)!;
-    this.serverName = this.server.name;
-    this.serverStatus = this.server.status;
     this.route.queryParams.subscribe(
       (queryParams: Params) => {
         this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
       }
     )
+    const id = this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id)!;
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  canDeactivate(): boolean | Promise<boolean> | Observable<boolean> 
+  {
+    if(!this.allowEdit){
+      return true;
+    }
+    if((this.serverName !== this.server.name) || (this.serverStatus !== this.server.status) && !this.changesSaved){
+      return confirm('Do you want to discard the changes ?');
+    }
+    else{
+      return true;
+    }
+  }
 }
