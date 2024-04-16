42239c2d0eaa91c4f06c07b5494aae8e54187d12
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-04-16 18:09:29 +0530

    Adding navigation, marking active routes

--- Angular - The complete guide/project1/src/app/header/header.component.ts
+++ Angular - The complete guide/project1/src/app/header/header.component.ts
@@ -1,17 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { DropdownDirective } from '../shared/dropdown.directive';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [DropdownDirective],
+  imports: [DropdownDirective, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @Output() showRecipes = new EventEmitter<string>();
+  // @Output() showRecipes = new EventEmitter<string>();
 
-  showFeature(feature: string) {
-    this.showRecipes.emit(feature);
-  }  
+  // showFeature(feature: string) {
+  //   this.showRecipes.emit(feature);
+  // }  
 }
