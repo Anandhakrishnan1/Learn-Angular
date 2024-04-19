6e9f81e0cefe6718398c97c8b867f5d5599bba22
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-04-19 13:14:36 +0530

    Adding editing routes

--- Angular - The complete guide/project1/src/app/app.routes.ts
+++ Angular - The complete guide/project1/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 export const routes: Routes = [
     {
@@ -14,8 +15,20 @@ export const routes: Routes = [
                 component: RecipeStartComponent
             },
             {
+                path: 'new',
+                component: RecipeEditComponent
+            },
+            {
                 path: ':id',
                 component: RecipeDetailComponent
+            },
+            {
+                path: ':id',
+                component: RecipeDetailComponent
+            },
+            {
+                path: ':id/edit',
+                component: RecipeEditComponent
             }
         ]
     },
