9707bb6b9d179e60f038ccae864838bafcee0bd8
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-04-23 15:06:29 +0530

    started learning observables

--- Angular - The complete guide/Observables/src/app/home/home.component.ts
+++ Angular - The complete guide/Observables/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  firstSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.firstSubscription = interval(1000).subscribe(
+      count => {
+        console.log(count);
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();
+  }
 
 }
