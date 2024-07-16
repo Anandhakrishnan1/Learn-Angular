faf6bb97b01f8ec559bad26635a010642d977c6f
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-07-16 15:42:49 +0530

    created the list view

--- Angular - Projects/ListView/src/app/manage-student/manage-student.component.ts
+++ Angular - Projects/ListView/src/app/manage-student/manage-student.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { StudentService } from '../../../public/assets/dummyData/student.service';
 
 @Component({
   selector: 'app-manage-student',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent
+          , CommonModule
+          , MatCardModule
+          , MatTableModule
+          , MatListModule],
   templateUrl: './manage-student.component.html',
   styleUrl: './manage-student.component.scss'
 })
-export class ManageStudentComponent {
+export class ManageStudentComponent implements OnInit {
+  students = new MatTableDataSource<any>([]);
+  displayedColumns: string[] = ['id', 'name', 'school', 'phone', 'email', 'country', 'status'];
+
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.students.data = this.studentService.getStudents()
+  }
 
 }
